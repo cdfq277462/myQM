70475a897be5dbabf57bb9bf5251b89269b7ab2b
cdfq277462 <610523009@gms.ndhu.edu.tw>
2020-05-21 15:13:04 +0800

    test

--- mathtools.cpp
+++ mathtools.cpp
@@ -11,7 +11,6 @@ float mathtools:: A_per(QString current_feedOut_Center, QString DataIn)
     int DataIn_Num                  = QString(DataIn).toInt();
 
     return (DataIn_Num - current_feedOut_Center_Num) / current_feedOut_Center_Num;
-
 }
 
 float mathtools::CV_per(QString DataIn, QString)
